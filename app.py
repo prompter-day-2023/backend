2f7857a677ac68d350e2ca94b35880e78d31b7a5
yu-heejin <jkgt0521@naver.com>
2023-08-25 18:38:21 +0900

    :sparkles: Feat: 이미지  윤곽선 코드 추가

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
 from flask import Flask
-app = Flask(__name__)
+import cv2
+import numpy as np
+from flask import jsonify, request
 
+# app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
 def hello():
-    return "Hello World!"
+    # file = request.files['file']
+    image = cv2.imread('./aaa.jpeg')
+
+    gray_img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(gray_img, 100, 250)
+
+    # 배경색과 엣지색을 지정
+    background_color = (255, 255, 255)  # 흰색 배경
+    edge_color = (0, 0, 0)  # 검은색 엣지
+
+    # 배경 부분을 원하는 배경색으로 채우기
+    filled_image = np.full_like(image, background_color)
+    filled_image[edged != 0] = edge_color
+
+    # cv2.imshow(filled_image)
+
+    # 엣지 확장을 위한 커널 생성
+    kernel = np.ones((5, 5), np.uint8)
+    dilated = cv2.dilate(edged, kernel, iterations=1)
+
+    # 배경색과 엣지색을 지정
+    background_color = (255, 255, 255)  # 흰색 배경
+    edge_color = (0, 0, 0)  # 검은색 엣지
+
+    # 배경 부분을 원하는 배경색으로 채우기
+    filled_image = np.full_like(image, background_color)
+    filled_image[dilated != 0] = edge_color
+
+    # cv2.imshow(filled_image)
+    cv2.imwrite('result.png', filled_image)
+
+hello()
