fe2639565525d8fce959f7b2b5e64410d82a1736
yu-heejin <jkgt0521@naver.com>
2023-08-25 19:31:48 +0900

    :sparkles: Feat: 사진 request 및 결과물 생성 코드 작성

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 from flask import Flask
+from flask import request
 import cv2
 import numpy as np
-from flask import jsonify, request
+from datetime import datetime
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
+@app.route('/', methods=['POST'])
 def hello():
-    # file = request.files['file']
-    image = cv2.imread('./aaa.jpeg')
+    file = request.files['file']
+    file_name = file.filename.split('.')[0]
+    image_type = file.filename.split('.')[-1]
+    created_at = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    new_file_name = f"{file_name}-{created_at}.{image_type}"
 
-    gray_img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 파일 데이터를 읽어와 NumPy 배열로 변환
+    file_data = file.read()
+    np_array = np.frombuffer(file_data, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edged = cv2.Canny(gray_img, 100, 250)
 
-    # 배경색과 엣지색을 지정
-    background_color = (255, 255, 255)  # 흰색 배경
-    edge_color = (0, 0, 0)  # 검은색 엣지
-
-    # 배경 부분을 원하는 배경색으로 채우기
-    filled_image = np.full_like(image, background_color)
-    filled_image[edged != 0] = edge_color
-
-    # cv2.imshow(filled_image)
-
     # 엣지 확장을 위한 커널 생성
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((2, 2), np.uint8)
     dilated = cv2.dilate(edged, kernel, iterations=1)
 
     # 배경색과 엣지색을 지정
@@ -35,7 +34,9 @@ def hello():
     filled_image = np.full_like(image, background_color)
     filled_image[dilated != 0] = edge_color
 
-    # cv2.imshow(filled_image)
-    cv2.imwrite('result.png', filled_image)
+    cv2.imwrite(new_file_name, filled_image)
 
-hello()
+    return { "status": 200, "message": 'OK' }
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
