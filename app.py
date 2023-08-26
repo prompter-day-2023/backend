16d505a890d370e8374686bd43666677b79b901b
yu-heejin <jkgt0521@naver.com>
2023-08-26 10:04:31 +0900

    :bug: Fix: 응답 값이 빈 칸인 문제 수정

--- app.py
+++ app.py
@@ -17,22 +17,19 @@ openai.api_key = os.getenv('GPT_API_KEY')
 def createDiary():
     contents = request.json.get('contents')
 
-    # TODO: GPT 요약 프롬프트 연결하기
     response = openai.Completion.create(
         model = 'text-davinci-003',   # openai에서 제공하는 모델 입력 (GPT-3.5)
-        prompt = '오늘은 친구들과 떡볶이를 먹었어',  # 원하는 실행어 입력
+        prompt = '오늘 하루 어떠신가요?\n',  # 원하는 실행어 입력
         temperature = 0,
         max_tokens = 100,   # 입력 + 출력 값으로 잡을 수 있는 max_tokens 값
-        top_p = 1,
         frequency_penalty = 0.0,
-        presence_penalty = 0.0,
-        stop = ['\n']   # stop 지점 설정
+        presence_penalty = 0.0
     )
     # TODO: DALLE 연결하기
 
     print(response);
 
-    return {'response': response.choices[0].text.strip()}
+    return { "response": response.choices[0].text.strip() }
 
 
 @app.route('/result', methods=['POST'])
