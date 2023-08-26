31511c79aaf0e124831d63fe1ce5f88e7da5883f
yu-heejin <jkgt0521@naver.com>
2023-08-27 03:04:29 +0900

    :sparkles: Feat: s3 버킷 연결 및 이미지 저장 예시 코드 작성

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request
 import cv2
 import numpy as np
 import openai
 import os
 from datetime import datetime
 from dotenv import load_dotenv
+from .s3_bucket import s3
 
 load_dotenv()
 
 app = Flask(__name__)
 
 openai.api_key = os.getenv('GPT_API_KEY')
+bucket_name = os.getenv('BUCKET_NAME')
 
 @app.route('/diary', methods=['POST'])
 def create_diary():
     contents = request.json.get('contents')
-    commend = f"Based on the diary contents written by the child, please write the diary contents and situation in English according to the format below. The purpose is to create an image by putting a prompt into the generative AI.\n\nEmotion:\nSubject:\nPicture color:\nOne line summary:\n\nThe diary contains the following.\n{contents}"
+    command = f'Based on the diary contents written by the child, please write the diary contents and situation in English according to the format below. The purpose is to create an image by putting a prompt into the generative AI.\n\nEmotion:\nSubject:\nPicture color:\nOne line summary:\n\nThe diary contains the following.\n{contents}'
 
     response = openai.Completion.create(
         model = 'text-davinci-003',   # openai에서 제공하는 모델 입력 (GPT-3.5)
-        prompt = commend,  # 원하는 실행어 입력
+        prompt = command,  # 원하는 실행어 입력
         temperature = 0,
         max_tokens = 300,   # 입력 + 출력 값으로 잡을 수 있는 max_tokens 값
         frequency_penalty = 0.0,
@@ -33,7 +35,24 @@ def create_diary():
 
     return { "response": response.choices[0].text.strip() }
 
+# S3 테스트 코드입니다. 향후 코드 작성 시 참고해주세요.
+@app.route('/s3-test')
+def upload_s3():
+    file = request.files['file']
+    file_name = file.filename.split('.')[0]
+    file_type = file.filename.split('.')[-1]
+
+    # 사진을 s3에 저장
+    s3.put_object(
+            Body = file,
+            Bucket = bucket_name,
+            Key = f'result/{file_name}.{file_type}',
+            ContentType = f'image/{file_type}'
+    )
+    return 'success'
+
 def create_line_picture(image):
+    # TODO: s3 url을 다운받도록 수정
     image_name = image.filename.split('.')[0]
     image_type = image.filename.split('.')[-1]
     created_at = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
@@ -61,8 +80,7 @@ def create_line_picture(image):
 
     cv2.imwrite(new_file_name, filled_image)
 
-    return { "status": 200, "message": 'OK' }
-
+    byte_image_to_s3 = cv2.imencode(image_type, filled_image)[1].tobytes()
 
 
 if __name__ == '__main__':
