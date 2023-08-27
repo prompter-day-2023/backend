97048bc13ecf9adde08643f4642c143628e7b85f
yu-heejin <jkgt0521@naver.com>
2023-08-27 15:21:28 +0900

    :sparkles: Feat: s3 url로 사진 라인 드로잉 변환 코드 추가

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import jsonify, request
+from datetime import datetime
+from dotenv import load_dotenv
+from .s3_bucket import s3
 import cv2
 import numpy as np
 import openai
 import os
-from datetime import datetime
-from dotenv import load_dotenv
-from .s3_bucket import s3
+import wget
 
 load_dotenv()
 
@@ -53,19 +54,20 @@ def upload_s3():
     )
     return f'{bucket_url_prefix}/result/{file_name}.{file_type}'
 
-def create_line_picture(image):
-    # TODO: s3 url을 다운받도록 수정
-    image_name = image.filename.split('.')[0]
-    image_type = image.filename.split('.')[-1]
+@app.route('/convert-test', methods=['POST'])
+def create_line_picture():
+    image_url = request.json.get('imageUrl')
+    download_image = wget.download(image_url)
+    image_file = cv2.imread(download_image)
+
+    # 파일명 생성하기
+    split_url = image_url.split('/')[-1]
+    image_name = split_url.split('.')[0]
+    image_type = split_url.split('.')[-1]
     created_at = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     new_file_name = f"{image_name}-{created_at}.{image_type}"
 
-    # 파일 데이터를 읽어와 NumPy 배열로 변환
-    image_data = image.read()
-    np_array = np.frombuffer(image_data, np.uint8)
-    np_image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-
-    gray_img = cv2.cvtColor(np_image, cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
     edged = cv2.Canny(gray_img, 100, 250)
 
     # 엣지 확장을 위한 커널 생성
@@ -77,12 +79,12 @@ def create_line_picture(image):
     edge_color = (0, 0, 0)  # 검은색 엣지
 
     # 배경 부분을 원하는 배경색으로 채우기
-    filled_image = np.full_like(image, background_color)
+    filled_image = np.full_like(image_file, background_color)
     filled_image[dilated != 0] = edge_color
 
     cv2.imwrite(new_file_name, filled_image)
 
-    byte_image_to_s3 = cv2.imencode(image_type, filled_image)[1].tobytes()
+    return { "status": 200, "message": 'OK' }
 
 
 if __name__ == '__main__':
